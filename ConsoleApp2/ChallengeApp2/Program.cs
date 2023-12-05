d2a05118fa8b7139647f7c2416e1ca73f02e9da8
Daniel <Daniel@DESKTOP-VQ44TAG>
2023-12-06 00:46:21 +0100

    D5

--- ConsoleApp2/ChallengeApp2/Program.cs
+++ ConsoleApp2/ChallengeApp2/Program.cs
@@ -1,68 +1,114 @@
-﻿string name = "Ew";
-int age = 33;
-bool male = false;
+﻿int number = 123337845;
 
-if (male == true)
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    if (age >= 18)
-    {
-        Console.WriteLine("Pelnoletni Mezczyzna");
+    if (letter == '0')
+    { 
+        counter0++;
     }
-    else
+    else if (letter == '1')
     {
-        Console.WriteLine("Niepelnoletni Mezczyzna");
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
     }
 }
 
-if (male == false)
-{
-    if (age < 30)
-    {
-        Console.WriteLine("Kobieta ponizej 30 lat");
-    }
-    if (age == 33 && name == "Ewa")
-    {
-        Console.WriteLine("Ewa, lat 33");
-    }
-    else
-    {
-        Console.WriteLine("Kobieta w wieku conajmniej 30 lat");
-    }
-}
+Console.WriteLine("Wyniki dla liczby: " + number);
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
 
 
-//int myAge = 30;
-//int newAge = myAge + 5;
-//float myNumber = 0;
-//double myNumber2 = 2;
 
 
-//// zmienne tekstowe
-//string name = "Daniel";
-//string surname = "Jankowski";
-//char myVar = 'a'; // zmienna z tylko jednym znakiem
+// TABLICE
 
-//string result = name + surname;
-//Console.WriteLine(result);
-
-
-//// zmienne logiczne
-//bool isActive = false;   // przyjmuje tylko dwie wartości (true/false)
-
-//var isValid = 5 > 6;
-//Console.WriteLine(isValid);
-
-// if
-// ==
-// <
-// >
-// <=
-// >=
-// !=
-
-// && - i
-// || - lub
-// ! - negacja
+//int[] grades = new int[5];
+//string[] dayOfWeeks = new string[7];
+//dayOfWeeks[0] = "poniedzialek";
+//dayOfWeeks[1] = "wtorek";
+//dayOfWeeks[2] = "sroda";
+//dayOfWeeks[3] = "czwartek";
+//dayOfWeeks[4] = "piatek";
+//dayOfWeeks[5] = "sobota";
+//dayOfWeeks[6] = "niedziela";
+//Console.WriteLine(dayOfWeeks[3]);
 
 
-// czyszczenie kodu - ctrl + k + d
+//for(int i=0; i<dayOfWeeks.Length; i++)
+//{
+//    Console.WriteLine(dayOfWeeks[i]);
+//}
+
+// LISTY
+
+//List<string> dayOfWeeks = new List<string>();
+//dayOfWeeks.Add("poniedzialek");
+//dayOfWeeks.Add("wtorek");
+//dayOfWeeks.Add("sroda");
+//dayOfWeeks.Add("czwartek");
+//dayOfWeeks.Add("piatek");
+//dayOfWeeks.Add("sobota");
+//dayOfWeeks.Add("niedziela");
+
+////for (int i = 0; i < dayOfWeeks.Count; i++)
+////{
+////    Console.WriteLine(dayOfWeeks[i]);
+////}
+
+//foreach (string day in dayOfWeeks)
+//{
+//    Console.WriteLine(day);
+//}
